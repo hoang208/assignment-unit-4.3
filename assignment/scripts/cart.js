112503669823aba1c818bdc5a3c52d219e3e8560
Paul HoangLong <paulhoanglong@gmail.com>
2023-08-03 15:49:43 -0500

    Completed required questions.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,13 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = []
+function addItem(item) {
+    basket.push(item)
+    return true
+}
 
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
 
-
-
-
-
-
+function listItems(basket) {
+    for (let i = 0; i<basket.length; i++) {
+        console.log(basket[i])
+    }
+}
+addItem("oranges")
+addItem("strawberries")
+addItem("pears")
+console.log(listItems(basket))
+console.log(`Basket is now ${basket}`);
 
 
 
